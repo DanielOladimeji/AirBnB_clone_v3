1103393e29626bfedf3643f9593c6e43b8019024
DanielOladimeji <danieloladimejiblessing@gmail.com>
2023-08-25 18:03:25 -0700

    done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 Contains the class DBStorage
 """
 
-import models
+import models  # noqa
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -12,7 +12,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from os import getenv
-import sqlalchemy
+import sqlalchemy  # noqa
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
